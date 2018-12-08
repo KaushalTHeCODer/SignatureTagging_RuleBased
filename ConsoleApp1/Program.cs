538a981ed80d202636a67918a5c376b19d458283
Kaushal <hotairballonflyinghigh@gmail.com>
2018-12-08 12:13:03 +0530

    firstCommit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using iTextSharp;
+using System.Text;
+using System.Threading.Tasks;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using iTextSharp.text;
+using System.IO;
+using iTextSharp.xtra.iTextSharp.text.pdf.pdfcleanup;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+       // public static int pgno = 0;
+        
+       // public static float height = 0;
+        static void Main(string[] args)
+        {
+            string filename = "2018-09-05_03-27-41-PM_f032c166f449443780c9f55aa746f7db";
+            XMLParserForNuance parser = new XMLParserForNuance();
+            List<Dictionary<string, string>> coordinates =  parser.ReadXml(filename);
+
+           //PdfReader pdfReader = new PdfReader("C:\\Users\\kprajapati\\Documents\\Projects\\SignatureTagging\\PdftoworkOn\\PdfToWorkOn2.pdf");
+            int GlobalIteration = 0;
+            
+
+            
+
+            foreach (Dictionary<string,string> cordinate in coordinates)
+            {
+                try
+                {
+
+
+                    float left_nu = float.Parse(cordinate["l"]);
+                    float top_nu = float.Parse(cordinate["t"]);
+                    float right_nu = float.Parse(cordinate["r"]);
+                    float bottom_nu = float.Parse(cordinate["b"]);
+                    int pgn = Convert.ToInt32(cordinate["PageNo"]);
+                    
+                    PdfReader pdfReader = new PdfReader("C:\\Users\\kprajapati\\Documents\\Projects\\SignatureTagging\\PdftoworkOn\\Output\\"+filename+".pdf");
+
+
+                    float page_width_nu = float.Parse(cordinate["Width"]);
+                    float page_height_nu = float.Parse(cordinate["Height"]);
+
+                    float page_width_itext = pdfReader.GetPageSize(pgn).Width;
+                    float page_height_itext = pdfReader.GetPageSize(pgn).Height;
+
+                    float relative_left = left_nu / page_width_nu;
+                    float relative_right = right_nu / page_width_nu + 0.01f;
+                    float relative_top = top_nu / page_height_nu;
+                    float relative_bottom = bottom_nu / page_height_nu - 0.02f;
+
+                    float itext_pos_left = page_width_itext - (relative_left * page_width_itext);
+                    float itext_pos_right = page_width_itext - (relative_right * page_width_itext);
+                    float itext_pos_top = page_height_itext - (relative_top * page_height_itext);
+                    float itext_pos_bottom = page_height_itext - (relative_bottom * page_height_itext);
+
+
+                    
+                    string file = "C:\\Users\\kprajapati\\Documents\\Projects\\SignatureTagging\\PdftoworkOn\\Output\\" + filename + ".pdf";
+                    //   string oldchar = "orignals.pdf"; string repChar = "copy.pdf";
+                    if (GlobalIteration == 0)
+                    {
+                        string repText = filename + GlobalIteration + ".pdf";
+                        Mark(file, filename+".pdf", repText, itext_pos_left, itext_pos_bottom, itext_pos_right, itext_pos_top, pgn);
+                        GlobalIteration++;
+                    }
+                    else
+                    {
+                        string orig = filename + (GlobalIteration - 1) + ".pdf";
+                        string repText = filename + GlobalIteration + ".pdf";
+                       
+                        var input1 = "C:\\Users\\kprajapati\\Documents\\Projects\\SignatureTagging\\PdftoworkOn\\Output\\" + orig;
+                        Mark(input1, orig, repText, itext_pos_left, itext_pos_bottom, itext_pos_right, itext_pos_top, pgn);
+                        File.Delete(input1);
+                        
+                        GlobalIteration++;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                  
+                }
+
+            }
+            
+            Console.ReadLine();
+
+        }
+        public static void Mark(string file,string oldFile,string newFile, float itext_pos_left, float itext_pos_bottom,float itext_pos_right,float itext_pos_top,int pgn)
+        {
+            PdfReader pdfReader = new PdfReader(file);
+            
+
+            PdfStamper stamper = new PdfStamper(pdfReader, new FileStream(file.Replace(oldFile, newFile), FileMode.Create, FileAccess.Write));
+
+            
+
+            List<PdfCleanUpLocation> cleanUpLocations = new List<PdfCleanUpLocation>();
+            cleanUpLocations.Add(new PdfCleanUpLocation(pgn, new iTextSharp.text.Rectangle(itext_pos_left, itext_pos_bottom, itext_pos_right, itext_pos_top), BaseColor.RED));
+            PdfCleanUpProcessor cleaner = new PdfCleanUpProcessor(cleanUpLocations, stamper);
+            cleaner.CleanUp();
+            stamper.Close();
+            pdfReader.Close();
+            stamper.Dispose();
+            pdfReader.Dispose();
+
+            
+        }
+       
+    }
+}
---
+++ ConsoleApp1/XMLParserForNuance.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ConsoleApp1
+{
+    class XMLParserForNuance
+    {
+        List<Dictionary<string, string>> cordinates = new List<Dictionary<string, string>>();
+        public void AddToMarkList(XmlNode xmlNode, string pgNo, string height, string width)
+        {
+            var AttrList = xmlNode.Attributes;
+            Dictionary<string, string> cordinate = new Dictionary<string, string>();
+            foreach (XmlAttribute attr in AttrList)
+            {
+                cordinate.Add(attr.Name.ToString(), attr.Value.ToString());
+            }
+            cordinate.Add("PageNo", pgNo);
+            cordinate.Add("Height", height);
+            cordinate.Add("Width", width);
+            cordinates.Add(cordinate);
+
+        }
+        public List<Dictionary<string, string>> ReadXml(string filename)
+        {
+            XmlDocument XDoc = new XmlDocument();
+            
+            XDoc.Load(@"C:\Users\kprajapati\Documents\Projects\SignatureTagging\XML files\" + filename+".xml");
+
+            XmlNodeList XList = XDoc.SelectNodes("//*");
+          //  List<Dictionary<string, string>> cordinates = new List<Dictionary<string, string>>();
+            List<Dictionary<string, string>> Page = new List<Dictionary<string, string>>();
+            List<Dictionary<string, string>> Metadata = new List<Dictionary<string, string>>();
+            string pgNo = "NOT";
+            string height = "NOT";
+            string width = "NOT";
+
+            for (int i = 0; i < XList.Count; i++)
+            {
+                
+                if(XList[i].Name == "source")
+                {
+                    var AttrList = XList[i].Attributes;
+                    Dictionary<string, string> metadata = new Dictionary<string, string>();
+                    foreach (XmlAttribute attr in AttrList)
+                    {
+                        if(attr.Name == "pageNo")
+                        {
+                            pgNo = attr.Value;
+                        }
+                        
+                        metadata.Add(attr.Name.ToString(), attr.Value.ToString());
+                       
+                    }
+                    Page.Add(metadata);
+                }
+                if (XList[i].Name == "theoreticalPage")
+                {
+                    var AttrList = XList[i].Attributes;
+                    Dictionary<string, string> metadata = new Dictionary<string, string>();
+                    foreach (XmlAttribute attr in AttrList)
+                    {
+                        if(attr.Name == "height")
+                        {
+                            height = attr.Value;
+                        }
+                        if(attr.Name == "width")
+                        {
+                            width = attr.Value;
+                        }
+                        metadata.Add(attr.Name.ToString(), attr.Value.ToString());
+                        
+                    }
+                    Metadata.Add(metadata);
+                }
+                
+                //Signatures(s) of Borrower(s)
+                //loan originator signature
+                if (XList[i].Name =="para" & (XList[i].InnerText.ToLower().StartsWith("signature(s)ofborrower(s)") |
+                    XList[i].InnerText.Contains("LoanOriginator'sSignature") | XList[i].InnerText.ToLower().StartsWith("signsignatureof")
+                    | XList[i].InnerText.ToLower() == "signature(seal)"))
+                {
+                  
+                    AddToMarkList(XList[i], pgNo, height, width);
+                }
+
+                //I ******* the undersigned 
+                    if (XList[i].InnerText.StartsWith("I") && XList[i].InnerText.ToLower().Contains("undersigned") 
+                    | XList[i].InnerText.StartsWith("InWitnessWhereof") )
+                {
+                   
+                    int k = i + 1;
+                    while (XList[k].Name != "rulerline")
+                    {
+                        k++;
+                    }
+                    if (XList[k].Name == "rulerline")
+                    {
+                        AddToMarkList(XList[k], pgNo, height, width);
+                       
+                    }
+                   // Console.WriteLine(XList[k+1].InnerText);
+                }
+                //witness my hands ---- signature
+                if (XList[i].InnerText.ToLower().StartsWith("signature"))
+                {
+                    int j = i-1;
+                    while(XList[j].Name != "para")
+                    {
+                        j--;
+                    }
+                  
+                    if (XList[j].Name == "para" & ( XList[j].InnerText.ToLower().StartsWith("witness")))
+                    {
+
+                        var AttrList = XList[i].Attributes;
+                        Dictionary<string, string> cordinate = new Dictionary<string, string>();
+                        foreach (XmlAttribute attr in AttrList)
+                        {
+                            cordinate.Add(attr.Name.ToString(), attr.Value.ToString());
+                        }
+                        cordinate.Add("PageNo", pgNo);
+                        cordinate.Add("Height", height);
+                        cordinate.Add("Width", width);
+                        cordinates.Add(cordinate);
+                    }
+                }
+                if(XList[i].Name == "table")
+                {
+                    foreach(XmlNode x in XList[i].ChildNodes)
+                    {
+                        //Console.WriteLine(x.Name);
+                        
+                        if(x.Name == "cell" && x.InnerText.ToLower().Contains("signature") )
+                        {
+                            //Console.WriteLine(x.InnerText);
+                            //Console.WriteLine(pgNo);
+                            if (x.HasChildNodes)
+                            {
+                               
+                                AddToMarkList(x.LastChild, pgNo, height, width);
+                            }
+                        }
+                        
+                    }
+                   
+                }
+
+                //Search for a line
+                if (XList[i].Name == "rulerline")
+                {
+                    int k = i - 1;
+                    while(XList[k].Name != "para")
+                    {
+                        k--;
+                    }
+                    
+                    //to handle cases for two signatures in a single line
+                    if (XList[i+1].Name == "rulerline")
+                    { 
+
+                        var para = XList[i+1].NextSibling;
+                        
+                        if (para != null && para.InnerText == "BorrowerSignatureDateBorrowerSignatureDate")
+                        {
+                            
+                            AddToMarkList(XList[i], pgNo, height, width);
+                            
+                            AddToMarkList(XList[i+1], pgNo, height, width);
+                        }
+                        if (para != null && para.InnerText == "Name:[SettlementAgentName]")
+                        {
+                            AddToMarkList(XList[i], pgNo, height, width);
+                        }
+                        //Console.WriteLine(para.InnerText);
+                    }
+                   
+
+                    //to handle cases where only single signature area is present
+                    else if (XList[i+1].Name == "para" | XList[i+1].Name == "section" 
+                        | XList[i + 1].Name == "dd")
+                    {
+                        var para = XList[i + 1];
+                        if (para != null && para.HasChildNodes)
+                        {
+                            foreach (XmlNode childNode in para.ChildNodes)
+                            {
+                                if (childNode.InnerText.EndsWith("Date") | childNode.InnerText.EndsWith("(Date)")
+                                    | childNode.InnerText.EndsWith("DATE")| childNode.InnerText.StartsWith("Veteran-")
+                                    | childNode.InnerText.StartsWith("Borrower") | childNode.InnerText.Contains("Signature") |
+                                    childNode.InnerText.StartsWith("Signature") | childNode.InnerText.StartsWith("Owner")|
+                                     childNode.InnerText.StartsWith("Grantor") | childNode.InnerText.StartsWith("Applicant")
+                                     | childNode.InnerText.ToLower().Contains("non-obligatedborrower")
+                                   | childNode.InnerText.StartsWith("Consumer")| childNode.InnerText.ToLower().Contains("non-borrowingspouse")
+                                    |childNode.InnerText == "OnbehalfoftheGrantees" | childNode.InnerText.Contains("Co-Borrower")
+                                   )
+                                {
+                                    AddToMarkList(XList[i], pgNo, height, width);
+                                    
+                                }
+                                //Console.WriteLine(childNode.Name);
+
+                                //Console.WriteLine(childNode.InnerText);
+                                //
+
+                            }
+                        }
+
+                    }
+
+                    if ( XList[k].InnerText.ToLower() == "affiant:" | XList[k].InnerText.ToLower() == "signature"
+                       // |XList[k].InnerText.ToLower() == "(seal)"| XList[k].InnerText.EndsWith("NOTICEOFRIGHTTOCANCEL.")
+                        )
+                    {
+                        AddToMarkList(XList[k], pgNo, height, width);
+                    }
+
+                }
+                //to handle cases where signature is present inside a frame "I WISH TO CANCEL"
+                else if (XList[i].Name == "frame")
+                {
+                    if (XList[i].HasChildNodes)
+                    {
+                        foreach(XmlNode xmlNodeChild in XList[i].ChildNodes)
+                        {
+                            if(xmlNodeChild.InnerText == "ConsumerSignatureDate" 
+                                | xmlNodeChild.InnerText == "IWISHTOCANCEL")
+                            {
+                                AddToMarkList(XList[i].LastChild, pgNo, height, width);
+                            }
+                        }
+                    }
+                }
+            }
+            //foreach(Dictionary<string,string> item in cordinates)
+            //{
+            //    foreach(var x in item.Values)
+            //    {
+            //        //Console.WriteLine(x.Key);
+            //        Console.WriteLine(x);
+            //    }
+            //    Console.WriteLine();
+            //}
+            //Console.ReadLine();
+            return cordinates;
+        }
+    }
+}
